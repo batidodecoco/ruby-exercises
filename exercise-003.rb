8a24055d43fae3a6aaf2a0fc5a4f771f6e7298fe
apacciaroni <apacciaroni@altimetrik.com>
2022-03-09 15:47:09 -0300

    Exercise 003

---
+++ exercise-003.rb
@@ -0,0 +1,13 @@
+def palindrome(word)
+	index = 0
+	word.split("").each do |char|
+		if char.downcase != word[word.length - 1 - index].downcase
+			return false
+		end
+		index += 1
+	end
+	return true
+end
+
+puts palindrome("racecar")
+puts palindrome("wagon")
